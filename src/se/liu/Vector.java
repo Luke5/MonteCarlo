00b1b10cf05c57efd6b3f59d8891ab14937fab2e
Unknown <lukas_adrian.haack@mailbox.tu-dresden.de>
2017-10-10 12:19:59 +0200

    Implemented basic Vector Functions

--- src/se/liu/Vector.java
+++ src/se/liu/Vector.java
@@ -1,37 +1,67 @@
 package se.liu;
+import  java.lang.Math.*;
 
 public class Vector {
-    public void setX(float x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public void setY(float y) {
+    public void setY(double y) {
         this.y = y;
     }
 
-    public void setZ(float z) {
+    public void setZ(double z) {
         this.z = z;
     }
 
-    public float getX() {
+    public double getX() {
 
         return x;
     }
 
-    public float getY() {
+    public double getY() {
         return y;
     }
 
-    public float getZ() {
+    public double getZ() {
         return z;
     }
 
-    public Vector(float x, float y, float z) {
+    public Vector(double x, double y, double z) {
 
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    private float x, y, z;
+    private double x, y, z;
+
+    public Vector add(Vector addedVec){
+        Vector sum = new Vector(this.x+addedVec.getX(),this.y+addedVec.getY(),this.z+addedVec.getZ());
+        return sum;
+    }
+
+    public Vector scalarProduct(double r){
+        Vector product = new Vector(this.x*r,this.y*r,this.z*r);
+        return product;
+    }
+
+    public double dotProduct( Vector vec){
+        return (this.x*vec.getX()+this.y+vec.getY()+this.z*vec.getZ());
+    }
+
+    public Vector crossProduct(Vector vec){
+        Vector product = new Vector(this.y*vec.getZ()-this.z*vec.getY(),this.z*vec.getX()-this.x*vec.getZ(),this.x*vec.getY()-this.y*vec.getX());
+        return product;
+    }
+
+    public double length(){
+        return Math.sqrt(this.x*this.x+this.y*this.y+this.z*this.z);
+    }
+
+    public Vector unitVector(){
+        double length =this.length();
+        Vector unit = new Vector(this.x/length,this.y/length,this.z/length);
+        return unit;
+    }
 }
